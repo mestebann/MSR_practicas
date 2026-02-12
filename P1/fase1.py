2e99cd2a9b5fab35ebbe078baaf449184920192e
mestebann <m.esteban.2023@alumnos.urjc.es>
2026-02-12 17:47:33 +0100

    Fase 1 terminada

--- P1/fase1.py
+++ P1/fase1.py
@@ -2,9 +2,11 @@ import pybullet as p
 import pybullet_data
 import time
 
-rampa_urdf = "rampa.urdf"
+# FICHEROS URDF
+ramp_urdf = "rampa.urdf"
 husky_urdf  = "husky/husky.urdf"
-plataforma_urdf = "plataforma.urdf"
+platform_urdf = "plataforma.urdf"
+barrier_urdf = "barrera.urdf"
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,28 +14,42 @@ p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
 
+# CONFIGURACION HUSKY
 husky_startPosition = [0,0,1]
 husky_euler_angles = [0,0,-1.57]
 husky_startOrientation = p.getQuaternionFromEuler(husky_euler_angles)
-rampa = p.loadURDF(husky_urdf, husky_startPosition, husky_startOrientation)
+husky = p.loadURDF(husky_urdf, husky_startPosition, husky_startOrientation)
 
-rampa_startPosition = [0,10,1]
-rampa_euler_angles = [0,0,-3.15]
+# CONFIGURACION RAMPA
+ramp_startPosition = [0,10,1]
+ramp_euler_angles = [0,0,-3.15]
+ramp_startOrientation = p.getQuaternionFromEuler(ramp_euler_angles)
+ramp = p.loadURDF(ramp_urdf, ramp_startPosition, ramp_startOrientation)
 
-rampa_startOrientation = p.getQuaternionFromEuler(rampa_euler_angles)
-rampa = p.loadURDF(rampa_urdf, rampa_startPosition, rampa_startOrientation)
+#CONFIGURACION BARRERA
+barrier_startPosition = [-1.5,17,1]
+barrier_euler_angles = [0,0,-1.5708]
+barrier_startOrientation = p.getQuaternionFromEuler(barrier_euler_angles)
+barrier = p.loadURDF(barrier_urdf, barrier_startPosition, barrier_startOrientation)
 
-plataforma_startPosition = [0,20,1]
-plataforma_euler_angles = [0,0,-3.15]
-plataforma_startOrientation = p.getQuaternionFromEuler(plataforma_euler_angles)
-plataforma = p.loadURDF(plataforma_urdf, plataforma_startPosition, plataforma_startOrientation)
+# CONFIGURACION PLATAFORMA
+platform_startPosition = [0,20,1]
+platform_euler_angles = [0,0,-3.15]
+platform_startOrientation = p.getQuaternionFromEuler(platform_euler_angles)
+platform = p.loadURDF(platform_urdf, platform_startPosition, platform_startOrientation)
 
+# JOINTS HUSKY
+husky_numJoints = p.getNumJoints(husky)
+husky_jointIndices = list(range(husky_numJoints))
 
 try:
     while True:
         p.stepSimulation()
         time.sleep(1./240.)
-
+        
+        # MOTORES HUSKY
+        p.setJointMotorControlArray(husky, husky_jointIndices, p.VELOCITY_CONTROL, targetVelocities = [-10, -10, -10, -10, -10, -10, -10, -10, -10, -10])
+        
 except KeyboardInterrupt:
     pass
 
